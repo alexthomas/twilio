1dceb68b95b2dfef6adf0414ac468a88b0a0f3e4
Alex Thomas <alex.c.thomas@gmail.com>
2013-11-06 16:06:34 +1100

    added twilio find method functionality

--- lib/twilio/message.rb
+++ lib/twilio/message.rb
@@ -2,14 +2,33 @@ module Twilio
 
     class Message < TwilioObject
       
+      @@path = 'SMS/Messages'
+      @@find_params = [:from,:to,:date_sent]
       attr_accessor :from, :to, :body, :media_url
       
       def initialize(params = {})
-        @path = 'SMS/Messages'
+        
         @options = params
         super
       end
-
+      
+      def self.build_twilio_params(params)
+        Rails.logger.debug "in message build twilio params"
+        twilio_params = params.with_indifferent_access
+        build_twilio_date_param!(twilio_params)
+        twilio_params = super(twilio_params)
+        
+      end
+      
+      def self.build_twilio_date_param!(twilio_params)
+        if !(date_keys = %w(sent_before sent_after sent_on_or_before sent_on_or_after) & twilio_params.keys).empty?
+          date_key = date_keys.first
+          comparator = date_key =~ /on/ ? '=' : ''
+          comparator = date_key =~ /before$/ ? "<#{comparator}" : ">#{comparator}"
+          twilio_params[:date_sent] = "DateSent#{comparator}#{twilio_params[date_key].to_s}"
+        end
+        twilio_params
+      end
     end
 
 end
--- lib/twilio/twilio_object.rb
+++ lib/twilio/twilio_object.rb
@@ -2,7 +2,11 @@ module Twilio
 
     class TwilioObject
       
-      attr_accessor :path,:options
+      cattr_accessor :path, :find_params
+      @@find_params = [:from,:to]
+      @@path = ''
+      
+      attr_accessor :path, :options
       
       def initialize(params = {})
         @options = params.with_indifferent_access 
@@ -10,10 +14,50 @@ module Twilio
       
       def self.create(options = {})
         to = self.new(options)
-        Twilio.post(to.path,to.options)
+        Twilio.post(self.path,to.options)
       end
       
-
+      def find(id)
+        self.new(Twilio.get("#{self.path}/#{id}"))
+      end
+      
+      #eventaully make recursive to get all not just last x
+      def self.all(params = {})
+        self.find_by(*params)
+      end
+      
+      def self.find_by(params = {})
+        return {}  if params.blank? || !params.is_a?(Hash)
+        twilio_params = build_twilio_params(params)
+        twilio_params
+        # response = Twilio.get(self.path,twilio_params)
+        #         resource_list = response.map do | resource |
+        #           self.new(resource)
+        #         end
+      end
+      
+      def self.build_twilio_params(params = {})
+        twilio_params = params.dup.with_indifferent_access
+        twilio_params.keys.each do | key |
+          downcase_key = key.downcase
+          underscore_key = downcase_key.underscore
+          if !self.find_params.include? underscore_key.to_sym
+            twilio_params.delete(key) 
+          else
+            if(key != downcase_key)
+              twilio_params[downcase_key] = twilio_params[key]
+              twilio_params.delete(key) 
+              key = downcase_key
+            end
+            if(key != underscore_key)
+              twilio_params[underscore_key] = twilio_params[key]; 
+              twilio_params.delete(key) 
+            end
+          end
+        end
+        twilio_params
+      end
+      
     end
 
 end
