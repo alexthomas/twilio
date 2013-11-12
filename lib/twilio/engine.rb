a307a1ef0c85e9bc2a8af509f46b944c3a15d005
Alex Thomas <alex.c.thomas@gmail.com>
2013-11-12 15:42:42 +1100

    removed twilio account information

--- lib/twilio/engine.rb
+++ lib/twilio/engine.rb
@@ -5,11 +5,11 @@ module Twilio
     initializer "accountable.load_app_instance_data" do |app|
       Twilio.setup do |config|
         config.app_root = app.root
-        config.twilio_account_sid = 'ACaf0eee667926566569d4ce3c9abc458e'
-        config.twilio_auth_token = '2229b2c0a5b6442d7c31a2c1791d6b92'
-        config.twilio_app_id = 'AP260515ea3b72ce619a7a4f97d999f172'
-        config.twilio_phone_number = '+15005550007';
-        config.twilio_test_number = '+61404100419';
+        # config.twilio_account_sid = ''
+        # config.twilio_auth_token = ''
+        # config.twilio_app_id = ''
+        # config.twilio_phone_number = '';
+        # config.twilio_test_number = '';
         
       end
     end
