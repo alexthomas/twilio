cffe1a59da04d1ab7a5d36dc8b71d7bc412d1430
Alex Thomas <alex.c.thomas@gmail.com>
2013-11-12 17:38:31 +1100

    remove indent from twiml generation

--- lib/twilio/twiml.rb
+++ lib/twilio/twiml.rb
@@ -2,7 +2,7 @@ module Twilio
   module TwiML
     
     def self.build &block
-      twiml = Builder::XmlMarkup.new(:indent => 2)
+      twiml = Builder::XmlMarkup.new()
       twiml.instance_eval do
         def method_missing(meth, *args, &blk)
           # Capitalise methods but keep parent builder generating xml
